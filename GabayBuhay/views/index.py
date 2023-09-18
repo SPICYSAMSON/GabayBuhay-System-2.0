88867509a04f1e31bb666ee7e8e861629afbff32
Third <iamzedthird@gmail.com>
2023-09-18 14:44:04 +0700

    indentation

--- GabayBuhay/views/index.py
+++ GabayBuhay/views/index.py
@@ -15,39 +15,39 @@ def index():
 def registration():
     if request.method == 'POST':
         with current_app.app_context():
-                bcrypt = current_app.config['BCRYPT']  # Retrieve the bcrypt instance from the app config
-                user_data = request.form.to_dict(flat=False)  # Convert to a regular dictionary
-                password = user_data['password'][0]  # Get the raw password from the form
-                repeat_password = user_data['repeat_password'][0]
-                
-                # Hash the password using Flask-Bcrypt 
-                hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-                hashed_repeat_password = bcrypt.generate_password_hash(repeat_password).decode('utf-8')
-                # Replace the raw password with the hashed password in the user_data dictionary
-                user_data['password'][0] = hashed_password
-                user_data['repeat_password'][0] = hashed_repeat_password
-                
-                # Handle file upload
-                if 'clinician_license' in request.files:
-                    file = request.files['clinician_license']
-                    if file.filename != '':
-                        filename = secure_filename(file.filename)
-                        file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
-                        user_data['clinician_license'] = filename  # Save the filename to the database
-                        
-                db.register_user(user_data)
-                #Turn the user_data into a regular dictionary!! 
-                user_data = {key: value[0] for key, value in user_data.items()}
-                
-                # Store user data in the session
-                session['user_data'] = user_data
-                
-                #Redirects the user to his/her respective page based on their role (patient/clinician)
-                if user_data['account_type'] == '1':
-                    return redirect(url_for('patient_bp.patient_home'))
-                elif user_data['account_type'] == '2':
-                    return redirect(url_for('clinician_bp.clinician_home'))
-                
+            bcrypt = current_app.config['BCRYPT']  # Retrieve the bcrypt instance from the app config
+            user_data = request.form.to_dict(flat=False)  # Convert to a regular dictionary
+            password = user_data['password'][0]  # Get the raw password from the form
+            repeat_password = user_data['repeat_password'][0]
+            
+            # Hash the password using Flask-Bcrypt 
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            hashed_repeat_password = bcrypt.generate_password_hash(repeat_password).decode('utf-8')
+            # Replace the raw password with the hashed password in the user_data dictionary
+            user_data['password'][0] = hashed_password
+            user_data['repeat_password'][0] = hashed_repeat_password
+            
+            # Handle file upload
+            if 'clinician_license' in request.files:
+                file = request.files['clinician_license']
+                if file.filename != '':
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+                    user_data['clinician_license'] = filename  # Save the filename to the database
+                    
+            db.register_user(user_data)
+            #Turn the user_data into a regular dictionary!! 
+            user_data = {key: value[0] for key, value in user_data.items()}
+            
+            # Store user data in the session
+            session['user_data'] = user_data
+            
+            #Redirects the user to his/her respective page based on their role (patient/clinician)
+            if user_data['account_type'] == '1':
+                return redirect(url_for('patient_bp.patient_home'))
+            elif user_data['account_type'] == '2':
+                return redirect(url_for('clinician_bp.clinician_home'))
+            
 
 @home_bp.route('/log_in', methods=['POST'])
 def log_in():
